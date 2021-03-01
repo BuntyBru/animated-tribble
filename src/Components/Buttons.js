10c660c3d147b553a9fc6dd0bb8f581f6a88e6b3
BuntyBru <buntybru@gmail.com>
2021-03-02 01:23:00 +0530

    buttons ✌️✌️✌️

--- src/Components/Buttons.js
+++ src/Components/Buttons.js
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-const PrimaryButton = styled.button``;
+const PrimaryBlue = "#0066ff9c";
+
+const Button = styled.button`
+  min-width: 100px;
+  padding: 12px 24px;
+  border-radius: 2px;
+  cursor: pointer;
+  font-family: "Roboto-Mono", monospace;
+`;
+
+const PrimaryButton = styled(Button)`
+  background-color: ${PrimaryBlue};
+  border: none;
+  font-size: 1rem;
+  color: white;
+`;
+
+export const SecondaryButton = styled(Button)`
+  background: none;
+  border: 2px solid ${PrimaryBlue};
+  color: ${PrimaryBlue};
+`;
+
+export const TertiaryButton = styled(SecondaryButton)`
+  border: none;
+`;
+
+export default PrimaryButton;
---
+++ src/Components/ChildrenComps/Hello.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PrimaryButton, { SecondaryButton, TertiaryButton } from "../Buttons";
+
+function Hello() {
+  return (
+    <div>
+      <PrimaryButton>Submit</PrimaryButton>
+      <SecondaryButton>Submit</SecondaryButton>
+      <TertiaryButton>Submit</TertiaryButton>
+    </div>
+  );
+}
+
+export default Hello;
